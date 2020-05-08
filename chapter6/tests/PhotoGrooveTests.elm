cf626c4991053063c6153133123e265fd00aa3d5
Barry Moore <chiroptical@gmail.com>
2020-05-07 20:14:28 -0400

    Finished chapter 6

--- chapter6/tests/PhotoGrooveTests.elm
+++ chapter6/tests/PhotoGrooveTests.elm
@@ -7,6 +7,7 @@ import Json.Decode exposing (decodeValue)
 import Json.Encode as Encode exposing (int, object, string)
 import PhotoGroove exposing (..)
 import Test exposing (..)
+import Test.Html.Event as Event
 import Test.Html.Query as Query
 import Test.Html.Selector as Selector
 
@@ -121,20 +122,22 @@ thumbnailRendered url query =
         |> Query.count (Expect.equal 1)
 
 
+makeImageNamesFromCount : Int -> List String
+makeImageNamesFromCount count =
+    List.range 1 count
+        |> List.map (\num -> String.fromInt num ++ ".png")
+
+
+photoFromUrl : String -> Photo
+photoFromUrl url =
+    { url = url, size = 0, title = "" }
+
+
 thumbnailsWork : Test
 thumbnailsWork =
-    fuzz (Fuzz.intRange 1 5) "Urls rendered as thumbnails" <|
-        \urlCount ->
+    fuzz (urlFuzzer 1 5) "Urls rendered as thumbnails" <|
+        \urls ->
             let
-                urls : List String
-                urls =
-                    List.range 1 urlCount
-                        |> List.map (\num -> String.fromInt num ++ ".png")
-
-                photoFromUrl : String -> Photo
-                photoFromUrl url =
-                    { url = url, size = 0, title = "" }
-
                 thumbnailChecks : List (Query.Single msg -> Expectation)
                 thumbnailChecks =
                     List.map thumbnailRendered urls
@@ -145,31 +148,27 @@ thumbnailsWork =
                 |> Expect.all thumbnailChecks
 
 
+getAt : Int -> List String -> String
+getAt n xs =
+    if n < 0 then
+        ""
+
+    else
+        case List.head <| List.drop n xs of
+            Nothing ->
+                ""
+
+            Just x ->
+                x
+
+
 thumbnailsLoadedFirstSelected : Test
 thumbnailsLoadedFirstSelected =
-    fuzz (Fuzz.intRange 1 5) "GotPhotos should return correct selected url" <|
-        \urlCount ->
+    fuzz (urlFuzzer 1 5) "GotPhotos should return correct selected url" <|
+        \urls ->
             let
-                urls : List String
-                urls =
-                    List.range 1 urlCount
-                        |> List.map (\num -> String.fromInt num ++ ".png")
-
-                photoFromUrl : String -> Photo
-                photoFromUrl url =
-                    { url = url, size = 0, title = "" }
-
-                getFirstUrl : List String -> String
-                getFirstUrl l =
-                    case l of
-                        h :: _ ->
-                            h
-
-                        _ ->
-                            ""
-
                 firstUrl =
-                    getFirstUrl urls
+                    getAt 0 urls
 
                 photos =
                     List.map photoFromUrl urls
@@ -181,34 +180,48 @@ thumbnailsLoadedFirstSelected =
                 |> Expect.equal (Loaded photos firstUrl)
 
 
+urlFuzzer : Int -> Int -> Fuzzer (List String)
+urlFuzzer start stop =
+    Fuzz.intRange start stop
+        |> Fuzz.map makeImageNamesFromCount
+
+
 thumbnailsClickPhotoWorks : Test
 thumbnailsClickPhotoWorks =
-    fuzz (Fuzz.intRange 2 5) "ClickPhoto should return correct selected url" <|
-        \urlCount ->
+    fuzz (urlFuzzer 2 5) "ClickPhoto should return correct selected url" <|
+        \urls ->
             let
-                urls : List String
-                urls =
-                    List.range 1 urlCount
-                        |> List.map (\num -> String.fromInt num ++ ".png")
-
-                photoFromUrl : String -> Photo
-                photoFromUrl url =
-                    { url = url, size = 0, title = "" }
-
-                getSecondUrl : List String -> String
-                getSecondUrl l =
-                    case l of
-                        _ :: s :: _ ->
-                            s
-
-                        _ ->
-                            ""
-
                 secondUrl =
-                    getSecondUrl urls
+                    getAt 1 urls
             in
             { initialModel | status = Loaded (List.map photoFromUrl urls) "" }
                 |> update (ClickPhoto secondUrl)
                 |> Tuple.first
                 |> .status
                 |> Expect.equal (Loaded (List.map photoFromUrl urls) secondUrl)
+
+
+clickThumbnail : Test
+clickThumbnail =
+    fuzz3 (urlFuzzer 1 1) Fuzz.string (urlFuzzer 1 1) "Clicking a thumbnail selects it" <|
+        \urlsBefore urlToSelect urlsAfter ->
+            let
+                url =
+                    urlToSelect ++ ".png"
+
+                photos =
+                    (urlsBefore ++ url :: urlsAfter)
+                        |> List.map photoFromUrl
+
+                srcToClick =
+                    prefix ++ url
+
+                prefix =
+                    "http://elm-in-action.com/"
+            in
+            { initialModel | status = Loaded photos "" }
+                |> view
+                |> Query.fromHtml
+                |> Query.find [ Selector.tag "img", Selector.attribute (Attr.src srcToClick) ]
+                |> Event.simulate Event.click
+                |> Event.expect (ClickPhoto url)
